f1caaeffe0f22f42cb7e6949c0c06de2b27abdac
Marcus Paulo <contato@iconcept.com.br>
2021-03-30 20:24:20 -0300

    first commit

---
+++ CadastroSeriesConsole/Entities/Serie.cs
@@ -0,0 +1,57 @@
+﻿using CadastroSeriesConsole.Enumerator;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroSeriesConsole.Entities
+{
+    public class Serie
+    {
+        public int Id { get; set; }
+        public Genero Genero { get; set; }
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public int Ano { get; set; }
+        public bool Excluido { get; set; }
+
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public override string ToString()
+        {
+            // Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+            return retorno;
+        }
+
+        public string RetornarTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public int RetornarId()
+        {
+            return this.Id;
+        }
+        public bool RetornarExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
---
+++ CadastroSeriesConsole/Repository/SerieRepositorio.cs
@@ -0,0 +1,87 @@
+﻿using CadastroSeriesConsole.Entities;
+using CadastroSeriesConsole.Interfaces;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace CadastroSeriesConsole.Services
+{
+    class SerieRepositorio : IRepositorio<Serie>
+    {
+        private List<Serie> listaSerie = new List<Serie>();
+        const string fileName = "series.json";
+
+        public SerieRepositorio()
+        {
+        }       
+
+        public void Atualizar(int id, Serie entidade)
+        {
+            var json = File.ReadAllText(Path.GetFullPath(fileName));
+            listaSerie = JsonConvert.DeserializeObject<List<Serie>>(json);
+            var indexUpdate = listaSerie.FindIndex(s => s.Id == id);
+            listaSerie[indexUpdate] = entidade;
+            UpdateFileJson(listaSerie);
+        }
+
+        public void Excluir(int id)
+        {
+            var json = File.ReadAllText(Path.GetFullPath(fileName));
+            listaSerie = JsonConvert.DeserializeObject<List<Serie>>(json);
+            var indexUpdate = listaSerie.FindIndex(s => s.Id == id);
+            listaSerie[indexUpdate].Excluido = true;
+            UpdateFileJson(listaSerie);
+        }
+
+        public void Inserir(Serie entidade)
+        {
+            List<Serie> listaSerie = new List<Serie>();
+            var json = File.ReadAllText(Path.GetFullPath(fileName));
+            listaSerie = JsonConvert.DeserializeObject<List<Serie>>(json);         
+            listaSerie.Add(entidade);
+            UpdateFileJson(listaSerie);
+        }
+
+        public List<Serie> Listar()
+        {
+            List<Serie> items;
+            using (StreamReader r = new StreamReader(Path.GetFullPath(fileName)))
+            {
+                string json = r.ReadToEnd();
+                items = JsonConvert.DeserializeObject<List<Serie>>(json);
+            };
+            return items;
+        }
+
+        public int ProximoId()
+        {
+            int Id = 0;
+            var json = File.ReadAllText(Path.GetFullPath(fileName));
+            listaSerie = JsonConvert.DeserializeObject<List<Serie>>(json);
+            foreach(Serie serie in listaSerie){
+               if (serie.Id > Id)
+                {
+                    Id = serie.Id;
+                }
+            }
+            return Id + 1;
+        }
+
+        public Serie RetornarPorId(int id)
+        {
+            var json = File.ReadAllText(Path.GetFullPath(fileName));
+            listaSerie = JsonConvert.DeserializeObject<List<Serie>>(json);
+            var viewObj = listaSerie.FirstOrDefault(s => s.Id == id);       
+            return viewObj;
+        }
+        void UpdateFileJson(List<Serie> listaSerie)
+        {
+            string novoJsonResult = Newtonsoft.Json.JsonConvert.SerializeObject(listaSerie, Newtonsoft.Json.Formatting.Indented);
+            File.WriteAllText(fileName, novoJsonResult);
+        }
+    }
+}
